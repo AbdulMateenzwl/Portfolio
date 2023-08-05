068b9f0bc5fa247e317b4e74478194d05da91a64
Abdul Mateen <abdulmateenkhanzwl@gmail.com>
2023-08-05 23:33:29 +0500

    header bar

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,48 @@
+import Link from 'next/link';
+import { BsThreeDots } from 'react-icons/hi';
+import {
+	RiFacebookBoxFill,
+	RiInstagramFill,
+	RiLinkedinFill,
+	RiGithubFill,
+	RiMore2Line,
+} from 'react-icons/ri';
+
 const Socials = () => {
-  return <div>Socials</div>;
+	return (
+		<div className='flex items-center gap-x-5 text-lg'>
+			<Link
+				href={'/'}
+				className='hover:text-accent transition-all duration-300'
+			>
+				<RiFacebookBoxFill />
+			</Link>
+			<Link
+				href={'/'}
+				className='hover:text-accent transition-all duration-300'
+			>
+				<RiInstagramFill />
+			</Link>
+			<Link
+				href={'/'}
+				className='hover:text-accent transition-all duration-300'
+			>
+				<RiLinkedinFill />
+			</Link>
+			<Link
+				href={'/'}
+				className='hover:text-accent transition-all duration-300'
+			>
+				<RiGithubFill />
+			</Link>
+			<Link
+				href={'/'}
+				className='hover:text-accent transition-all duration-300'
+			>
+				<RiMore2Line />
+			</Link>
+		</div>
+	);
 };
 
 export default Socials;
